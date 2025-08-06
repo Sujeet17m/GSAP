1448339a9894c1f004ab5cd9253b7d19b74db6dc
Sujeet17m <sujeet17das@gmail.com>
2025-08-06 12:07:46 +0530

    transition

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
-let tl = gsap.timeline(); // lowercase 'timeline', not 'timeLine'
+let tl = gsap.timeline();
 
-tl.from("#nav1",{
-    y: -100,
-    opacity: 0,
-    duration: 1,
-    delay: 0.5,
-    stagger: 0.5,
-    ease: "power2.out"
-})
-tl.from(".nav2",{
-    y: -100,
-    opacity: 0,
-    duration: 1,    
-    delay: 0.5,
-    stagger: 0.5,
-})
+tl.from("#nav1", {
+  y: -100,
+  opacity: 0,
+  duration: 1,
+  delay: 0.5,
+  ease: "power2.out"
+});
+
+tl.from(".nav2 h1", {
+  y: -80,
+  opacity: 0,
+  duration: 1,
+  stagger: 0.3,
+  ease: "power2.out"
+});
+
+
+
+tl.from(".main > h1", {
+  y: 50,
+  opacity: 0,
+  duration: 1,
+  stagger: 0.2,
+  ease: "power2.out"
+});
+
+tl.from(".main > h2", {
+  x: -50,
+  opacity: 0,
+  duration: 1,
+  stagger: 0.2,
+  ease: "power2.out"
+});
+
+tl.from(".main > p", {
+  scale: 0,
+  opacity: 0,
+  duration: 1,
+  ease: "back.out(1.7)"
+});
+
