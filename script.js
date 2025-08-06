f9976a71df67ba05cd4b20b0cf101fe2a1df918a
Sujeet17m <sujeet17das@gmail.com>
2025-08-06 11:21:34 +0530

    heading

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 let tl = gsap.timeline(); // lowercase 'timeline', not 'timeLine'
 
-tl.from("#box", {
-  opacity: 0.5,
-  backgroundColor: "rgb(21, 126, 21)",
-  duration: 1,
-  x: 100,
-  y: 100,
-  rotation: 360,
-  scale: 0.5,
-  delay: 0.5
-});
+tl.from("#nav1",{
+    y: -100,
+    opacity: 0,
+    duration: 1,
+    delay: 0.5,
+    stagger: 0.5,
+    ease: "power2.out"
+})
+tl.from(".nav2",{
+    y: -100,
+    opacity: 0,
+    duration: 1,    
+    delay: 0.5,
+    stagger: 0.5,
+})
